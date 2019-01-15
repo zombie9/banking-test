546412f1c083ea4035d91eb4c4cb56a4934df08a
Darryl Banks <darryl.banks@zombie9.com>
2019-01-15 17:34:32 +0000

    add transactions array to Statement

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,5 +1,9 @@
 class Statement
 
+  attr_reader :transactions
 
+  def initialize(transactions = [])
+    @transactions = transactions
+  end
 
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,5 +1,9 @@
 require 'statement'
 
 describe Statement do
-  
+
+  it 'should start with an empty list of transactions' do
+    expect(subject.transactions.length).to eq 0
+  end
+
 end
